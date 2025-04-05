3ec12318511463d6586cbc134f76c2bb690f59b8
Nahom Tewodros <nahomtewodrosm@gmail.com>
2025-04-06 02:43:52 +0300

    im soooog good

--- app/api/contact/[id]/route.ts
+++ app/api/contact/[id]/route.ts
@@ -1,12 +1,13 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  const { id } = req.url; // Extract the dynamic ID from the URL
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // Extract the dynamic ID from the URL
 
   // Assuming ID is extracted from the URL, let's check it
   if (id) {
     return NextResponse.json({ message: `Received ID: ${id}` });
   } else {
-    return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
   }
 }

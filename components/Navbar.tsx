a4a8e0e581e5a5052c57a7902bed098eb93a0753
Nahom Tewodros <nahomtewodrosm@gmail.com>
2025-04-30 22:52:11 +0300

    alke qemem tech project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -178,7 +178,7 @@ export default function Navbar() {
             className="flex items-center justify-center rounded-lg bg-transparent text-white p-1 "
             whileHover={{ rotate: 5 }}
           >
-            <Image className="rounded-lg" src="/natqmem.jpg" alt="Qmem Logo" width={400} height={40} />
+            <Image className="rounded-lg" src="/natqmem.jpg" alt="Qmem Logo" width={40} height={40} />
           </motion.div>
           <span className="font-bold text-xl text-blue-800 dark:text-blue-200">
             Qmem Cloud
